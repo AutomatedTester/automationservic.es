ffef14c33ab469946999fb0c6dd20653f5b2fd5e
AutomatedTester <dburns@mozilla.com>
2011-09-04 15:01:16 +0100

    clean up of unnecessary GET

--- app.js
+++ app.js
@@ -55,13 +55,6 @@ app.get('/irc', function(req, res){
     client.quit()
 });
 
-app.get('/sockets', function(req, res){
-    
-});
-
-
-
-
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
